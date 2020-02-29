90b9d42c00d9e1fe361cdbcb2b6cc1177dd6b655
Free322 <56409820+Free322@users.noreply.github.com>
2020-02-29 14:52:59 +0300

    devby
    task

---
+++ WindowsApplication/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace WindowsApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button = new System.Windows.Forms.Button();
+            this.zedGraphControl = new ZedGraph.ZedGraphControl();
+            this.koefa = new System.Windows.Forms.TextBox();
+            this.koefb = new System.Windows.Forms.TextBox();
+            this.koefc = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.koefx = new System.Windows.Forms.TextBox();
+            this.koefxend = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnexit = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button.Location = new System.Drawing.Point(103, 335);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(102, 29);
+            this.button.TabIndex = 0;
+            this.button.Text = "Построить";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // zedGraphControl
+            // 
+            this.zedGraphControl.Location = new System.Drawing.Point(234, 12);
+            this.zedGraphControl.Name = "zedGraphControl";
+            this.zedGraphControl.ScrollGrace = 0;
+            this.zedGraphControl.ScrollMaxX = 0;
+            this.zedGraphControl.ScrollMaxY = 0;
+            this.zedGraphControl.ScrollMaxY2 = 0;
+            this.zedGraphControl.ScrollMinX = 0;
+            this.zedGraphControl.ScrollMinY = 0;
+            this.zedGraphControl.ScrollMinY2 = 0;
+            this.zedGraphControl.Size = new System.Drawing.Size(404, 352);
+            this.zedGraphControl.TabIndex = 1;
+            // 
+            // koefa
+            // 
+            this.koefa.Location = new System.Drawing.Point(103, 44);
+            this.koefa.MaxLength = 2;
+            this.koefa.Name = "koefa";
+            this.koefa.Size = new System.Drawing.Size(52, 20);
+            this.koefa.TabIndex = 2;
+            // 
+            // koefb
+            // 
+            this.koefb.Location = new System.Drawing.Point(103, 70);
+            this.koefb.MaxLength = 2;
+            this.koefb.Name = "koefb";
+            this.koefb.Size = new System.Drawing.Size(52, 20);
+            this.koefb.TabIndex = 3;
+            // 
+            // koefc
+            // 
+            this.koefc.Location = new System.Drawing.Point(103, 96);
+            this.koefc.MaxLength = 2;
+            this.koefc.Name = "koefc";
+            this.koefc.Size = new System.Drawing.Size(52, 20);
+            this.koefc.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Коэффициент \"а\"";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Коэффициент \"b\"";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Коэффициент \"c\"";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Граница переменной х";
+            // 
+            // koefx
+            // 
+            this.koefx.Location = new System.Drawing.Point(44, 170);
+            this.koefx.MaxLength = 2;
+            this.koefx.Name = "koefx";
+            this.koefx.ReadOnly = true;
+            this.koefx.Size = new System.Drawing.Size(52, 20);
+            this.koefx.TabIndex = 9;
+            this.koefx.Text = "0";
+            // 
+            // koefxend
+            // 
+            this.koefxend.Location = new System.Drawing.Point(142, 170);
+            this.koefxend.MaxLength = 2;
+            this.koefxend.Name = "koefxend";
+            this.koefxend.Size = new System.Drawing.Size(52, 20);
+            this.koefxend.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "от";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(117, 173);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "до";
+            // 
+            // btnexit
+            // 
+            this.btnexit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnexit.Location = new System.Drawing.Point(12, 335);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(80, 29);
+            this.btnexit.TabIndex = 14;
+            this.btnexit.Text = "Выход";
+            this.btnexit.UseVisualStyleBackColor = true;
+            this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Синий",
+            "Красный",
+            "Зеленый",
+            "Черный"});
+            this.comboBox1.Location = new System.Drawing.Point(103, 214);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(91, 21);
+            this.comboBox1.TabIndex = 15;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(22, 217);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Выбор цвета:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 376);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnexit);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.koefxend);
+            this.Controls.Add(this.koefx);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.koefc);
+            this.Controls.Add(this.koefb);
+            this.Controls.Add(this.koefa);
+            this.Controls.Add(this.zedGraphControl);
+            this.Controls.Add(this.button);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Построение параболы";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+        private ZedGraph.ZedGraphControl zedGraphControl;
+        private System.Windows.Forms.TextBox koefa;
+        private System.Windows.Forms.TextBox koefb;
+        private System.Windows.Forms.TextBox koefc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox koefx;
+        private System.Windows.Forms.TextBox koefxend;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnexit;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ WindowsApplication/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace WindowsApplication
+{
+    public partial class Form1 : Form
+    {
+        int a, b, c;
+        int x = 0;
+        int z = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreateGraph(zedGraphControl);
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                a = int.Parse(koefa.Text);
+                b = int.Parse(koefb.Text);
+                c = int.Parse(koefc.Text);
+                x = int.Parse(koefx.Text);
+                z = int.Parse(koefxend.Text);
+                CreateGraph(zedGraphControl);
+            }
+            catch
+            {
+                MessageBox.Show("Неверные значения коэффициентов!");
+                koefa.Clear();
+                koefb.Clear();
+                koefc.Clear();
+                koefx.Clear();
+                koefxend.Clear();
+                koefa.Focus();
+            }
+        }
+
+        private Color color;
+        private void CreateGraph(ZedGraphControl zgc)
+        {            
+            GraphPane myPane = zgc.GraphPane;
+            myPane.Title.Text = "График параболы";
+            myPane.XAxis.Title.Text = "Ось X";
+            myPane.YAxis.Title.Text = "Ось Y";
+            myPane.CurveList.Clear();
+
+            double x1, y;
+            
+            PointPairList list1 = new PointPairList();
+            PointPairList list2 = new PointPairList();
+
+            for (int i = x; i <= z; i++)
+            {
+                    x1 = i;
+                    y = a * Math.Pow(x1, 2) + b * x1 + c;
+                    list1.Add(x1, y);
+                    list2.Add(-x1, y);
+            }
+            LineItem myCurve = myPane.AddCurve("sun", list1, color, SymbolType.Circle); // отрисовываем график
+            LineItem my1Curve = myPane.AddCurve("", list2, color, SymbolType.None); // отрисовываем график
+            myCurve.Line.IsVisible = false;
+
+            // Задаем вид пунктирной линии для крупных рисок по оси X:
+            myPane.XAxis.MajorGrid.IsVisible = true;
+            myPane.XAxis.MajorGrid.DashOn = 10;
+            myPane.XAxis.MajorGrid.DashOff = 1;
+            // Задаем вид пунктирной линии для крупных рисок по оси Y:
+            myPane.YAxis.MajorGrid.IsVisible = true;
+            myPane.YAxis.MajorGrid.DashOn = 10;
+            myPane.YAxis.MajorGrid.DashOff = 1;
+            myPane.XAxis.Scale.Min = 0;
+            myPane.XAxis.Scale.Max=100;
+            zgc.AxisChange();
+            zgc.Refresh();
+
+                     {
+        SaveFileDialog savefile = new SaveFileDialog();
+        savefile.DefaultExt = ".json";
+            savefile.Filter = "Test Files|*.json";
+                if (savefile.ShowDialog() == System.Windows.Forms.DialogResult.OK);
+             
+        }
+            
+            
+        }
+
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if ((string)comboBox1.SelectedItem == "Синий") 
+            {
+                color = Color.Blue;
+            }
+            if ((string)comboBox1.SelectedItem == "Красный") 
+            {
+                color = Color.Red;
+            }
+            if ((string)comboBox1.SelectedItem == "Зеленый")
+            {
+                color = Color.Green;
+            }
+            if ((string)comboBox1.SelectedItem == "Черный")
+            {
+                color = Color.Black;
+            }
+        }
+    }
+}
+
